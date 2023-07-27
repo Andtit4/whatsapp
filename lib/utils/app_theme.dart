2ae07694ebe5163d2ee058d68b68487b45913490
Andtit4 <=>
2023-07-27 08:57:59 +0000

    add puglins

---
+++ lib/utils/app_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  static final TextTheme fontTheme = GoogleFonts.poppinsTextTheme();
+  static final Color primaryColor = Color(0xff41C85A);
+  static final Color secondaryColor = Color(0xff62C975);
+  static final Color backgroundColor = Color(0xff1d1d1d);
+}
+/* 
+final ThemeData appTheme = ThemeData(
+    textTheme: GoogleFonts.poppinsTextTheme(), primaryColor: Color(0xffE7E708)); */
